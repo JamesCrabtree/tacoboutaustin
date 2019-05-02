39b1f463dba10e2fe32d28b82b1cd36b22e3a0c0
James Crabtree <jcrabtree@sparkcognition.com>
2019-05-01 21:23:32 -0500

    test backend push

--- app/backend/tacoapi.py
+++ app/backend/tacoapi.py
@@ -19,7 +19,7 @@ import re
 numberOfClosePlace = 4
 
 dayDict = {"Sunday": 0, "Monday": 1, "Tuesday": 2,
-           "Wednesday": 3, "Thursday": 4, "Friday": 5, "Saturday": 6}
+           "Wednesday": 3, "Thursday": 4, "Friday": 5, "Saturday": 6} 
 
 def getOtherType(type):
     if type == "restaurant":
