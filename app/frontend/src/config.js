ebc4930435b16f92c8cd5d731222b0fdf41a4c2a
James Crabtree <jcrabtree@sparkcognition.com>
2019-05-06 16:18:11 -0500

    try to get app to run in production mode

--- app/frontend/src/config.js
+++ app/frontend/src/config.js
@@ -1,3 +1,3 @@
-// export const api_url = "http://localhost";
-export const api_url = "http://backend-deployment"; 
+// export const api_url = "http://localhost:5000";
+export const api_url = process.env.BACKEND_DEPLOYMENT_SERVICE_HOST;
 
